f71cf0c5dee13d76741b3f9e3e162e108d4ba1ec
Vikas Kumar <vikas12252@gmail.com>
2025-06-22 19:27:36 +0530

    Update vite.config.js

--- chat-app-frontend/vite.config.js
+++ chat-app-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // your backend
+        target: 'https://chatapp-backend-x9hk.onrender.com', // your backend
         changeOrigin: true,
         secure: false,
       },
